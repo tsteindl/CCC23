248d7c56e4a8a777fa24d4f0624de1122bfbf257
Michael Mayr <47149083+MayrMichael@users.noreply.github.com>
2023-10-20 15:14:14 +0200

    added inputs

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def open_file(path):
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        map = list()
+        colist = list()
+        return map, colist
+
+def get_co(col, row, map):
+    return map[row][col]
+
 
 if __name__ == '__main__':
     print_hi('PyCharm')
