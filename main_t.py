7030e6335295a0a1710c7d45575e644770737f13
tsteindl <tobias.steindl@gmx.net>
2023-10-20 17:33:55 +0200

    add l4 files

--- main_t.py
+++ main_t.py
@@ -26,6 +26,26 @@ def check_same_island(map, c1, c2):
     res = search(map, visited, next_v, c2)
     return res
 
+
+def find_route(map, start, end):
+    res = []
+    visited = []
+    next_v = [start]
+
+    while len(next) > 0:
+        c = next_v.pop(0)
+        if map[c[0], c[1]] == "L":
+            continue
+        if c[0] >= len(map) or c[1] >= len(map[0]):
+            continue
+        if c == end:
+            return []
+        next_v = [
+            [c[0] + 1, c[1]], [c[0] + 1, c[1] + 1], [c[0], c[1] + 1], [c[0] - 1, c[1]], [c[0] - 1, c[1] - 1], [c[0], c[1] - 1], [c[0] + 1, c[1] - 1], [c[0] - 1, c[1] + 1],
+        ] + next_v
+        visited.append(c)
+
+
 def get_diag(c):
     return [c[0] + 1, c[1] + 1], [c[0] - 1, c[1] - 1], [c[0] + 1, c[1] - 1], [c[0] - 1, c[1] + 1]
 
@@ -41,23 +61,26 @@ def check_route(map, route):
             # for d in get_diag(v):
             #     if d in visited:
             #         return "INVALID"
-            if [c[0] + 1, c[1]] in visited and [c[0] + 1, c[1]] in visited
+            # if [c[0] + 1, c[1]] in visited and [c[0], c[1] + 1] in visited or [c[0] + 1, c[1] + 1] in visited and [c[0], c[1] - 1] in visited or [c[0] + 1, c[1]] in visited and [c[0], c[1] + 1] in visited
+
 
         visited.append(c)
     return res
 
+
+
+
 if __name__ == '__main__':
-    path = "in/level3/level3_example.in"
-    out_path = "out/level3/level3_example.out"
+    path = "in/level4/level4_example.in"
+    out_path = "out/level4/level4_example.out"
     with open(path, 'r') as f:
         res = ""
-
         map, coords = parse(f.read())
         print(map)
         print(coords)
-            # res += check_same_island(map, c1, c2)
         for route in coords:
-            res += check_route(map, route) + "\n"
+            # res += check_route(map, route) + "\n"
+            res += find_route(map, route[0], route[1]) + "\n"
         print(res)
         # for i, j in coords:
         #     res += (map[i][j]) + "\n"
